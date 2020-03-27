1a449543337f295c703a72df17d79522502b50ca
Chase Sullins <jcsullins222@yahoo.com>
2020-03-27 09:06:29 -0500

    updated about me

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = props => {
         <h1>ABOUT THE ARTIST</h1>
         <hr></hr>
       </section>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed vulputate odio ut enim blandit volutpat maecenas. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tincidunt lobortis feugiat vivamus at augue. Tincidunt ornare massa eget egestas purus viverra. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum. Justo laoreet sit amet cursus sit amet dictum sit. Faucibus ornare suspendisse sed nisi. At elementum eu facilisis sed odio morbi quis commodo odio. Ac turpis egestas maecenas pharetra convallis.</p>
+      <p>Hello there! My name is Chase Sullins. I'm currently a spray paint artists based in the Nashville area. I first started spray painting around 2016 after looking for a new medium to express myself through art. </p>
       <button
         type="button"
         className="btn"
