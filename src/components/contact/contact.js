847f4bbd57af174b8c328cbdab82d8d217296941
Chase Sullins <jcsullins222@yahoo.com>
2020-04-07 14:46:44 -0500

    removed some text from the contacts page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,14 +5,11 @@ const Contact = () => {
     <>
       <section className="section-content">
         <h1>CONTACT</h1>
-      <hr></hr>
+        <hr></hr>
       </section>
       <p>
-        <em>You can contact us via email: csART@csart.com
-</em>
+        <em>You can contact us via email: csART@csart.com</em>
       </p>
-      <h3>Commission</h3>
-      <em>commission info and form </em>
     </>
   );
 };
